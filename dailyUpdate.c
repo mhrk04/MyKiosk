10ba352494224c064276a08d80981b31b15724e8
mhrk04 <haziqrohaizan@gmail.com>
2023-06-16 08:54:55 +0800

    add profit at daily update

--- dailyUpdate.c
+++ dailyUpdate.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+
 #define MAX_ITEMS 20
 
 struct Item {
     char code[6];
     int quantity;
 };
+
+struct KioskItem {
+    char code[6];
+    char name[12];
+    int stock;
+    int lowLimit;
+    double costPrice;
+    double sellingPrice;
+};
+
+double calculateProfit(struct KioskItem* kioskItems, int numItems, struct Item* soldItems, int numSoldItems) {
+    double totalProfit = 0.0;
+
+    for (int i = 0; i < numSoldItems; i++) {
+        for (int j = 0; j < numItems; j++) {
+            if (strcmp(soldItems[i].code, kioskItems[j].code) == 0) {
+                double profit = (kioskItems[j].sellingPrice - kioskItems[j].costPrice) * soldItems[i].quantity;
+                totalProfit += profit;
+                break;
+            }
+        }
+    }
+
+    return totalProfit;
+}
 
 void displaySoldItems() {
     time_t currentTime = time(NULL);
@@ -37,16 +64,36 @@ void displaySoldItems() {
 
     fclose(transactionFile);
 
+    // Load kiosk items from the database
+    FILE* kioskFile = fopen("myKdata.ori", "rb");
+    if (kioskFile == NULL) {
+        printf("Error opening kiosk database file!\n");
+        return;
+    }
+
+    struct KioskItem kioskItems[MAX_ITEMS];
+    int numKioskItems = 0;
+
+    // Read all the kiosk items into an array
+    while (fread(&kioskItems[numKioskItems], sizeof(struct KioskItem), 1, kioskFile)) {
+        numKioskItems++;
+    }
+
+    fclose(kioskFile);
+
     // Print the sold items in reverse order (latest transaction at the top)
     for (int i = numItems - 1; i >= 0; i--) {
         printf("%-6s | %-8d\n", soldItems[i].code, soldItems[i].quantity);
         printf("------------------------\n");
     }
+
+    // Calculate and print the total profit
+    double totalProfit = calculateProfit(kioskItems, numKioskItems, soldItems, numItems);
+    printf("Total Profit: %.2lf\n", totalProfit);
 }
 
-
 int main() {
-  system("cls");
+    system("cls");
     displaySoldItems();
     printf("Press ENTER to go back to the menu");
     getchar();
--- menu.c
+++ menu.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// #include "Functions.h"
+
 
 int main() {
     int choice;
